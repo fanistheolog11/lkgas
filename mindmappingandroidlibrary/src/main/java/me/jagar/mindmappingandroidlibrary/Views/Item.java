c2c0009366bf19ed0bde68c45835e64753e6af91
fanistheolog11 <84383625+fanistheolog11@users.noreply.github.com>
2021-05-18 18:22:54 +0300

    Update Item.java

--- mindmappingandroidlibrary/src/main/java/me/jagar/mindmappingandroidlibrary/Views/Item.java
+++ mindmappingandroidlibrary/src/main/java/me/jagar/mindmappingandroidlibrary/Views/Item.java
@@ -112,10 +112,6 @@ public class Item extends LinearLayout {
         return this.title;
     }
     
-    public getText(){
-        return this.getTitle().getText();
-    }
-    
     public TextView getContent(){
         return this.content;
     }
